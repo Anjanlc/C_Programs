39effc93bef008181908a12e3487a61526245a26
Anjanlc <9845445539.al@gmail.com>
2023-07-07 21:22:29 +0545

    .

--- fibonacci_recursion.c
+++ fibonacci_recursion.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-int fib(int);
-int main()
-{
-    int n, i, j;
-    printf("\nEnter a value of n:");
-    scanf("%d",&n);
-    for(i=0; i<n; i++){
-        j=fib(i);
-        printf("\n%d",j);
-    }
-}
-int fib(int n)
-{
-    if(n==0)
-        return 0;
-    else
-        if(n==1)
-            return 1;
-            else
-                return fib(n-1) + fib(n-2);
-}
+// #include<stdio.h>
+// int fib(int);
+// int main()
+// {
+//     int n, i, j;
+//     printf("\nEnter a value of n:");
+//     scanf("%d",&n);
+//     for(i=0; i<n; i++){
+//         j=fib(i);
+//         printf("\n%d",j);
+//     }
+// }
+// int fib(int n)
+// {
+//     if(n==0)
+//         return 0;
+//     else
+//         if(n==1)
+//             return 1;
+//             else
+//                 return fib(n-1) + fib(n-2);
+// }
